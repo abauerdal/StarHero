fd812ca24001b79f249ed0cd220d247e635c7dee
Malcolm McLean <ml217327@dal.ca>
2025-11-13 21:20:19 -0400

    Removed RB from notes

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -13,12 +13,12 @@ public class Note : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb.linearVelocity = new Vector2(0, -speed);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.down * speed * Time.deltaTime;
     }
 }
